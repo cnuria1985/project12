4c381b9d8d68178f38fa649676a709b92d677673
нурия клементьева <cnuria@mail.ru>
2025-04-03 19:57:36 +0500

    masks and widget

--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,7 @@
 from typing import List, Dict, Any
-from typing import List [Dict[str, Any]]
 
 
-def filter_by_state(list_of_dictionaries: list, state: str = "EXECUTED") -> list:
+def filter_by_state(list_of_dictionaries: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Функция возвращает список словарей, содержащий только те словари, у которых ключ state"""
     total_of_dictionary = []
     for dictionary in list_of_dictionaries:
@@ -12,15 +11,8 @@ def filter_by_state(list_of_dictionaries: list, state: str = "EXECUTED") -> list
 
 
 
+def sort_by_date(list_of_dictionaries: list[dict], reverse: bool = True) -> list[dict]:
+    """Функция возвращает список словарей, отсортированный по дате"""
 
-from typing import List, Dict, Any
-from typing import List [Dict[str, Any]]
-
-
-def sort_by_date(list_of_dictionaries: list, reverse: bool = True) -> list:
-"""Функция возвращает список словарей, отсортированный по дате"""
-    for dictionary in list_of_dictionaries:
-        total_of_dictionary = sorted(
-        list_of_dictionaries, key=lambda x: x["date"], reverse=reverse
-            )
+    total_of_dictionary = sorted(list_of_dictionaries, key=lambda x: x["date"], reverse=reverse)
     return total_of_dictionary
